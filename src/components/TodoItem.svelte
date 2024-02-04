4046872b27ba9f38d9691a2a7ff81a0f70f1ba26
SasukeDaTony <tonytaylor@pursuit.org>
2024-02-04 10:55:11 -0500

    export todo items as seperate standalone component and imported into todoList page tested functionality all is working

---
+++ src/components/TodoItem.svelte
@@ -0,0 +1,64 @@
+<script>
+  export let index;
+  export let todo;
+  export let editTodo;
+  export let removeTodo;
+</script>
+
+<div class="todo">
+  <p>
+    {index + 1}. {todo}
+  </p>
+  <div class="actions">
+    <i
+      role="button"
+      tabindex="0"
+      class="fa-regular fa-pen-to-square"
+      on:click={() => {
+        editTodo(index);
+      }}
+      on:keydown={() => {}}><span class="square">Edit</span></i
+    >
+    <i
+      role="button"
+      tabindex="0"
+      class="fa-regular fa-trash-can"
+      on:click={() => {
+        removeTodo(index);
+      }}
+      on:keydown={() => {}}><span class="square">Delete</span></i
+    >
+  </div>
+</div>
+
+<style>
+  .todo {
+    border-left: 1px solid cyan;
+    padding: 4px 8px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    gap: 14px;
+  }
+
+  .fa-pen-to-square:hover {
+    color: #d3c722;
+    cursor: pointer;
+  }
+
+  .square {
+    transition-duration: 0ms;
+    padding: 5px;
+    font-family: "Times New Roman", Times, serif;
+  }
+
+  .fa-trash-can:hover {
+    color: rgba(226, 28, 28, 0.934);
+    cursor: pointer;
+  }
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -3,6 +3,7 @@
   import { authHandlers, authStore } from "../../store/store";
   import { getDoc, doc, setDoc } from "firebase/firestore";
   import { auth, db } from "../../lib-firebase/firebase";
+  import TodoItem from "../../components/TodoItem.svelte";
   let todoList = [];
   let currTodo = "";
   let error = false;
@@ -73,31 +74,7 @@
       {/if}
       {#each todoList as todo, index}
         {#if todo !== ""}
-          <div class="todo">
-            <p>
-              {index + 1}. {todo}
-            </p>
-            <div class="actions">
-              <i
-                role="button"
-                tabindex="0"
-                class="fa-regular fa-pen-to-square"
-                on:click={() => {
-                  editTodo(index);
-                }}
-                on:keydown={() => {}}><span class="square">Edit</span></i
-              >
-              <i
-                role="button"
-                tabindex="0"
-                class="fa-regular fa-trash-can"
-                on:click={() => {
-                  removeTodo(index);
-                }}
-                on:keydown={() => {}}><span class="square">Delete</span></i
-              >
-            </div>
-          </div>
+          <TodoItem {todo} {index} {removeTodo} {editTodo} />
         {/if}
       {/each}
     </main>
@@ -168,36 +145,6 @@
     flex-direction: column;
     gap: 8px;
     flex: 1;
-  }
-
-  .todo {
-    border-left: 1px solid cyan;
-    padding: 4px 8px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .actions {
-    display: flex;
-    align-items: center;
-    gap: 14px;
-  }
-
-  .fa-pen-to-square:hover {
-    color: #d3c722;
-    cursor: pointer;
-  }
-
-  .square {
-    transition-duration: 0ms;
-    padding: 5px;
-    font-family: "Times New Roman", Times, serif;
-  }
-
-  .fa-trash-can:hover {
-    color: rgba(226, 28, 28, 0.934);
-    cursor: pointer;
   }
 
   .enterTodo {
